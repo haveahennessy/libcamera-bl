5c5de6de2e0048a563bb107591788ce7ef039da4
Matt Isaacs <izzy@buglabs.net>
2011-06-23 17:36:22 -0400

    Initial commit.
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

---
+++ Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH:= $(call my-dir)
+
+ifneq ($(USE_CAMERA_STUB),true)
+#
+# libcamera
+#
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    CameraHardware.cpp \
+    V4L2Camera.cpp \
+    media.c \
+    converter.cpp
+
+LOCAL_SHARED_LIBRARIES:= \
+    libcutils \
+    libui \
+    libutils \
+    libbinder \
+    libjpeg \
+    libcamera_client \
+    libsurfaceflinger_client
+
+LOCAL_C_INCLUDES += \
+    external/jpeg
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE:= libcamera
+
+include $(BUILD_SHARED_LIBRARY)
+endif
+
+include $(all-subdir-makefiles)
